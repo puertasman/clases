05312c3a0651c56c7cf5f20e9e88f7204376d1c6
puertasman <puertasman@gmail.com>
2025-03-26 13:27:07 +0100

    list

--- html/peliculas/js/script.js
+++ html/peliculas/js/script.js
@@ -2,10 +2,17 @@ import { peliculas } from './peliculas.js';
 
 let contenedorPeliculas = document.querySelector("#peliculas")
 
-let listaPeliculas = document.createElement("ul");
-for (let pelicula of peliculas){
-    let item = document.createElement("li");
-    item.textContent = pelicula.titulo;
+let listaPeliculas = document.createElement("table");
+if (peliculas.length == 0){
+    let item = document.createElement("tr");
+    item.textContent = "No hay peliculas";
     listaPeliculas.appendChild(item);
+}
+else{
+    for (let pelicula of peliculas){
+        let item = document.createElement("tr");
+        item.innerHTML = `<td>${pelicula.titulo}</td><td>${pelicula.director}</td><td>${pelicula.año}</td><td><img src="${pelicula.img}" alt="${pelicula.titulo}"></td>`;
+        listaPeliculas.appendChild(item);
+}
 }
 contenedorPeliculas.appendChild(listaPeliculas);

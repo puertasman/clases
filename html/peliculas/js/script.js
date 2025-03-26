316340eb10449d0769096da4d55ed4ea0790692b
puertasman <puertasman@gmail.com>
2025-03-26 22:02:28 +0100

    lista y datos imdb

--- html/peliculas/js/script.js
+++ html/peliculas/js/script.js
@@ -2,45 +2,36 @@ import { peliculas } from './peliculas.js';
 
 
 const apiKey = "3be066e6";
-let contenedorPeliculas = document.querySelector("#peliculas")
+let contenedor = document.querySelector("#peliculas")
 
-let listaPeliculas = document.createElement("table");
 if (peliculas.length == 0){
-    let item = document.createElement("tr");
-    item.textContent = "No hay peliculas";
-    listaPeliculas.appendChild(item);
+    contenedor.textContent = "No hay peliculas";
 }
 else{
-    let cabecera = document.createElement("thead");
-    cabecera.innerHTML = `<tr><th>Título</th><th>Director</th><th>Género</th><th>Poster</th></tr>`;
-    listaPeliculas.appendChild(cabecera);
     peliculas.forEach(titulo => {
     fetch(`https://www.omdbapi.com/?t=${encodeURIComponent(titulo)}&apikey=${apiKey}`)
       .then(res => res.json())
       .then(data => {
         if (data.Response === "True") {
-          const div = document.createElement("tr");
-          div.className = "pelicula";
-          div.innerHTML = `
-            <td>${data.Title} (${data.Year})</td>
-            <td>${data.Director}</td>
-            <td>${data.Genre}</td>
-            <td><img src="${data.Poster}" alt="Poster de ${data.Title}"></td>
-          `
-          listaPeliculas.appendChild(div)
+            const col = document.createElement("div");
+            col.className = "col-md-4";
+    
+            col.innerHTML = `
+              <div class="card h-100 shadow-sm">
+                <img src="${data.Poster}" class="card-img-top" alt="Poster de ${data.Title}">
+                <div class="card-body">
+                  <h5 class="card-title">${data.Title} (${data.Year})</h5>
+                  <p class="card-text"><strong>Director:</strong> ${data.Director}</p>
+                  <p class="card-text"><strong>Género:</strong> ${data.Genre}</p>
+                  <p class="card-text"><strong>IMDb:</strong> ${data.imdbRating}</p>
+                </div>
+              </div>
+            `;
+            contenedor.appendChild(col);
         } else {
           console.warn(`No encontrada: ${titulo}`)
         }
       })
-      .catch(err => console.error("Error al cargar película:", err));
-  });
-/*
-else{
-    for (let pelicula of peliculas){
-        let item = document.createElement("tr");
-        item.innerHTML = `<td>${pelicula.titulo}</td><td>${pelicula.director}</td><td>${pelicula.año}</td><td><img src="${pelicula.img}" alt="${pelicula.titulo}"></td>`;
-        listaPeliculas.appendChild(item);
-}
-}*/
-    contenedorPeliculas.appendChild(listaPeliculas)
+      .catch(err => console.error("Error al cargar película:", err))
+  })
 }
